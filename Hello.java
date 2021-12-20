64808483dfd067347158243a2b813dbd702c7c8e
Karan <644230016@webmail.npru.ac.th>
2021-12-20 15:50:26 +0700

    second comment

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello IT");
+        System.out.println("Hello Karan");
+        System.out.println("Hello Khanthong");
     }
 }
